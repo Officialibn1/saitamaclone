bf11e5f2f96a69f9086f3dbcddcfe646b7743ae8
ibn <officialibn001@gmail.com>
2023-10-30 10:04:11 +0100

    Navbar complete

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,66 @@
-import React from 'react'
+'use client'
+import Link from 'next/link';
+import React, { useState } from 'react'
+import { 
+    FaBars, 
+    FaLinkedin, 
+    FaXTwitter, 
+    FaFacebook, 
+    FaReddit, 
+    FaGithubAlt, 
+    FaYoutube, 
+    FaTelegram, 
+    FaAnglesLeft
+} from "react-icons/fa6";
+
 
 const Navbar = () => {
+    
+    const [toggleNav, setToggleNav] = useState(false)
+
+    const toggler = () => {
+        setToggleNav((prev) => !prev);
+    }
+
   return (
     <nav>
 
+        {/* Navbar Links */}
+        <div className={`navLinks ${!toggleNav ? '-left-[200vw]' : 'left-0'}`} >
+            <ul>
+                <li><button onClick={toggler}><FaAnglesLeft /></button></li>
+                <li onClick={toggler} className='linksItem'><Link href={`/`} >Home</Link></li>
+                <li onClick={toggler} className='linksItem'><Link href={`/`} >Download Saita Pro</Link></li>
+                <li onClick={toggler} className='linksItem'><Link href={`/`} >Trade Crypto</Link></li>
+                <li onClick={toggler} className='linksItem'><Link href={`/`} >Buy Saitama</Link></li>
+            </ul>
+        </div>
+
         {/* Toggle Button for nav Links */}
-        <div className="toggleBtn">
-            
+        <div className="toggleBtn" onClick={toggler}>
+            <FaBars />
         </div>
 
         {/* Navbar logo Icon */}
         <div className="navLogo">
-
+            <img src='/logo.svg' alt='saitama website clone logo' />
         </div>
 
         {/* Navbar social media icons container */}
         <div className="mediaIcons">
+            <FaLinkedin />
+            
+            <FaXTwitter />
 
+            <FaFacebook />
+
+            <FaReddit />
+
+            <FaGithubAlt />
+
+            <FaYoutube />
+
+            <FaTelegram />
         </div>
     </nav>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className="text-4xl">
-      Saitama Inu. . .
-    </h1>
+    <section>
+
+      <h1 className="text-4xl">
+        Saitama Inu. . .
+      </h1>
+    </section>
   )
 }
