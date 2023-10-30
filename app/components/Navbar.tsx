a4f99401c8cec750bafb1c2ca97a751db13a26d6
ibn <officialibn001@gmail.com>
2023-10-30 08:34:33 +0100

    added image assets

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+
+        {/* Toggle Button for nav Links */}
+        <div className="toggleBtn">
+            
+        </div>
+
+        {/* Navbar logo Icon */}
+        <div className="navLogo">
+
+        </div>
+
+        {/* Navbar social media icons container */}
+        <div className="mediaIcons">
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
